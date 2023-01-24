61d1378fef920e3c6cb6e321c37881ef0d3bf53d
Michael L. Collard <collard@uakron.edu>
2023-01-24 15:03:26 -0500

    Extract avg()

--- rainfall.cpp
+++ rainfall.cpp
@@ -22,6 +22,17 @@ double max(const std::vector<double>& data) {
     return maximum;
 }
 
+// average of the data
+double avg(const std::vector<double>& data) {
+
+    auto total = data[0];
+    for (const auto value: data) {
+        total += value;
+    }
+
+    return total / data.size();
+}
+
 int main() {
 
     // input the hourly rainfall data
@@ -36,11 +47,7 @@ int main() {
     auto heaviest = max(rainfall);
 
     // calculate the average rainfall
-    auto total = rainfall[0];
-    for (const auto rain: rainfall) {
-        total += rain;
-    }
-    const auto average = total / rainfall.size();
+    const auto average = avg(rainfall);
 
     // output the rainfall report
     std::cout << "| Hourly Rainfall | Inches in 100s |" << '\n';
